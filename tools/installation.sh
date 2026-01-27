1e6086b350dd735e13f8462e83b6bdd7e3ef972f
mortega89 <landi.lorenzo@outlook.com>
2026-01-27 21:48:53 +0100

    feat(setup): Add Kubernetes development environment setup

---
+++ tools/installation.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+# Install kind
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.31.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+
+# Install FreeLens
+curl -LO https://github.com/freelensapp/freelens/releases/download/v1.8.0/Freelens-1.8.0-linux-amd64.rpm
+sudo rpm -i Freelens-1.8.0-linux-amd64.rpm
+rm Freelens-1.8.0-linux-amd64.rpm
+freelens version
+
+
+# Install Docker
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker --version
+
+
+# Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
+
+
+# Install k9s
+sudo dnf install k9s -y
+k9s version
+
+
+
+
+
+
+
